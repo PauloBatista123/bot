8d5102a5690e5962a6ce1a60e9b0587f35d017b1
Paulo Henrique <PAULOE4264_00@aracoop.local>
2023-02-14 16:15:28 -0300

    styles: update colors from operacoes and erros

--- resources/views/livewire/log/index.blade.php
+++ resources/views/livewire/log/index.blade.php
@@ -2,15 +2,11 @@
     @forelse ($this->logs as $log)
     @if ($loop->index === 0)
     <div style="
-        background: #F1F1E6;
+        background: #49479d;
         box-shadow: rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px;
-        color: #49479d;
+        color: white;
     " class="alert text-center rounded shadow-lg" role="alert">
-        <div class="d-flex flex-row justify-content-center align-items-center">
-            <i class="fa-regular fa-bell fa-2xl fa-bell fa-shake"></i>
-            <h2 class="alert-heading p-2">Atenção!</h2>
-        </div>
-        <hr>
+    <i class="fa-regular fa-bell fa-2xl fa-bell fa-shake"></i>
         <p class="fs-2 fw-bold">{{$log->mensagem}}</p>
         <hr>
         <span class="fs-5">{{\Carbon\Carbon::parse($log->created_at)->format('d/m/Y H:i')}}</span>
@@ -18,8 +14,8 @@
     </div>
     @else
     <div style="
-       background: #F1F1E6;
-       color: #49479d;
+       background: #49479d;
+       color: white;
     " class="alert text-center" role="alert">
         <p class="fs-3 font-weight-light">{{$log->mensagem}}</p>
         <hr>
--- resources/views/livewire/servico/contador.blade.php
+++ resources/views/livewire/servico/contador.blade.php
@@ -3,21 +3,23 @@
         <div class="col-6">
             <div style="
                 background: #00AE9D;
-                box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+                box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px !important;
                 color: white;
+                border: 2px solid white;
             " class="alert text-center rounded shadow-lg" role="alert">
-            <p class="fs-2 fw-bold">{{$numberOfServices}}</p>
-            <span class="fs-5">OPERAÇÕES</span>
+            <p class="fw-bold" style="font-size: 3.5rem">{{$numberOfServices}}</p>
+            <span class="fs-3">OPERAÇÕES</span>
             </div>
         </div>
         <div class="col-6">
             <div style="
                 background: #49479d;
-                box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px;
+                box-shadow: rgba(0, 0, 0, 0.56) 0px 22px 70px 4px !important;
                 color: white;
+                border: 2px solid white;
             " class="alert text-center rounded shadow-lg" role="alert">
-            <p class="fs-2 fw-bold">{{$numberOfErrors}}</p>
-            <span class="fs-5">ERROS</span>
+            <p class="fw-bold" style="font-size: 3.5rem">{{$numberOfErrors}}</p>
+            <span class="fs-3">ERROS</span>
             </div>
         </div>
     </div>
