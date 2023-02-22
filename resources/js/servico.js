02514d78d4dc0b7fb67f58d4b335e70777d37be8
gabriel <gabriel@aracoop.com.br>
2023-02-22 09:03:35 -0300

    servico bug

--- resources/js/servico.js
+++ resources/js/servico.js
@@ -3,11 +3,11 @@ import {Player, loaded} from 'tone'
 
 const channel = window.Echo.channel('private.servico');
 
-// const player = new Player("https://cdn.freesound.org/previews/674/674807_14714786-lq.mp3").toDestination();
+const player = new Player("https://cdn.freesound.org/previews/674/674807_14714786-lq.mp3").toDestination();
 
-// loaded().then(() => {
-//     console.log("Loaded");
-// });
+loaded().then(() => {
+    console.log("Loaded");
+});
 
 channel.subscribed(() => {
     console.log('conectado ao serviço...');
@@ -18,7 +18,7 @@ channel.subscribed(() => {
 
 }).listen('.novo-log', (event) => {
 
-    // player.start();
+    player.start();
     console.log('enviando...');
     window.livewire.emit('render_novo_log', event);
 
