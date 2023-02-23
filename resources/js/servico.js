e01b6f5476940b2d567998fe4690da48122f407c
Paulo Henrique <PAULOE4264_00@aracoop.local>
2023-02-23 15:18:19 -0300

    bug: correcao de bug do audio

--- resources/js/servico.js
+++ resources/js/servico.js
@@ -2,22 +2,36 @@ import './bootstrap';
 
 const channel = window.Echo.channel('private.servico');
 
+const success = new Audio(`${process.env.MIX_APP_URL}/sounds/success.mp3`);
+const log = new Audio(`${process.env.MIX_APP_URL}/sounds/logs.mp3`);
+log.muted = true;
+success.muted = true;
+
 channel.subscribed(() => {
     console.log('conectado ao serviço...');
-}).listen('.novo-servico', async (event) => {
+}).listen('.novo-servico', (event) => {
 
-    const success = new Audio(`${process.env.MIX_APP_URL}/sounds/success.mp3`);
-    success.play();
+    playSound('novo-servico');
 
     console.log('enviando novo serviço...');
     window.livewire.emit('render_novo_servico', event);
 
-}).listen('.novo-log', async (event) => {
 
-    const log = new Audio(`${process.env.MIX_APP_URL}/sounds/logs.mp3`);
-    log.play();
+}).listen('.novo-log', (event) => {
+
+    playSound('novo-log');
 
     console.log('enviando novo log...');
     window.livewire.emit('render_novo_log', event);
 
 });
+
+function playSound(sound){
+    if(sound === 'novo-log'){
+        log.muted = false;
+        log.play();
+    }else if(sound === 'novo-servico'){
+        success.muted = false;
+        success.play();
+    }
+}
