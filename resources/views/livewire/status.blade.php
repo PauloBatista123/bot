b0b62e0f826b2aa0db26377c9b67ea14d03ae4c2
Paulo Henrique <PAULOE4264_00@aracoop.local>
2023-02-24 15:15:47 -0300

    update: tamanho da font e icone do robo

--- resources/views/livewire/status.blade.php
+++ resources/views/livewire/status.blade.php
@@ -1,14 +1,14 @@
 <div>
     <div class="d-flex flex-column justify-content-center align-items-center">
         @if($this->status === 'active')
-        <img src="{{asset('imgs/success_obo.gif')}}" alt="" width="120" height="120">
-        <p class="text-white text-center" style="font-size: 9px;">Estou trabalhando na velocidade da luz...</p>
+        <img src="{{asset('imgs/success_obo.gif')}}" alt="" class="img-robo">
+        <p class="text-white text-center" style="font-size: 1.5rem;">Estou trabalhando na velocidade da luz...</p>
         @elseif($this->status === 'inactive')
-        <img src="{{asset('imgs/obo.gif')}}" alt="" width="120" height="120">
-        <p class="text-white text-center" style="font-size: 9px;">Recuperando as forças...</p>
+        <img src="{{asset('imgs/obo.gif')}}" alt="" class="img-robo">
+        <p class="text-white text-center" style="font-size: 1.5rem;">Recuperando as forças...</p>
         @elseif($this->status === 'error')
-        <img src="{{asset('imgs/obo_error.gif')}}" alt="" width="120" height="120">
-        <p class="text-white text-center" style="font-size: 9px;">Ahhh!! Erro... Chamando socorro...</p>
+        <img src="{{asset('imgs/obo_error.gif')}}" alt="" class="img-robo">
+        <p class="text-white text-center" style="font-size: 1.5rem;">Ahhh!! Erro... Chamando socorro...</p>
         @endif
     </div>
 </div>
